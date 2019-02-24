8a656e2eb6739caaa11bd5d2410cf25edd195f97
Prasad <prsn0001@synchronoss.com>
2019-02-24 22:37:50 +0530

    modified simple response

--- server/src/main/java/com/svmapp/server/Svmapp.java
+++ server/src/main/java/com/svmapp/server/Svmapp.java
@@ -4,16 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @SpringBootApplication
 public class Svmapp {
 
-	 @RequestMapping("/")
-	    @ResponseBody
-	    String home() {
-	      return "Hello World!";
-	    }
+	@RequestMapping("/")
+    public String home() {
+      return "Hello World!";
+    }
 	 
 	public static void main(String[] args) {
 		SpringApplication.run(Svmapp.class, args);
