9f91c359c37b98dcd76822dc55ed5fbb4afa5d90
Prasad <prsn0001@synchronoss.com>
2019-02-25 14:55:54 +0530

    na

--- server/src/main/java/com/svmapp/server/Svmapp.java
+++ server/src/main/java/com/svmapp/server/Svmapp.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @SpringBootApplication
 public class Svmapp {
 
 	@RequestMapping("/")
+	@ResponseBody
     public String home() {
       return "Hello World!";
     }
